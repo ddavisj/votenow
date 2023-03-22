009163a71bc267153c7f7dc88eb51fc0c5e58c5b
David Hack <davidhack11@gmail.com>
2023-03-22 23:27:27 +1100

    Updates before deploying (prep)

--- index.js
+++ index.js
@@ -25,5 +25,18 @@ app.use(passport.session());
 require('./routes/authRoutes')(app); // end of T31
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'prod') {
+   // Express will serve up prodn assets
+   // like our main.js or main.css files
+   app.use(express.static('client/build'));
+
+   // Express will serve up the index.html file
+   // if it doesn't recognise the route
+   const path = require('path');
+   app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+   });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
