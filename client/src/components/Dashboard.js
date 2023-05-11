6034b987a7a4b218a88d2dba7f886e9174e1433b
David Hack <davidhack11@gmail.com>
2023-05-12 01:20:27 +1000

    Almost done! Surveys now being rendered

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SurveyList from './surveys/SurveyList';
+// import * as actions from '../actions';
+// import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+
+// class Dashboard extends Component {
+//    componentDidMount() {
+//       this.props.fetchSurveys();
+//    }
+
+//    render(state) {
+//       console.log(state);
+//       return <div></div>;
+//    }
+// }
+
+// function mapStateToProps(state) {
+//    return state;
+// }
+
+// export default connect(mapStateToProps)(Dashboard);
 
 const Dashboard = () => {
    return (
       <div>
+         <SurveyList />
          <div className="fixed-action-btn">
             <Link to="/surveys/new" className="btn-floating btn-large red">
                <i className="material-icons">add</i>
