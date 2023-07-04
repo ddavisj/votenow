8c61d6c0b881de0e5bc6ddec35fd2236f45523a0
David Hack <davidhack11@gmail.com>
2023-07-04 12:55:22 +1000

    Converted Dashboard to func comp, cleaned up

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,14 +1,11 @@
-// import React from 'react';
-import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import SurveyList from './surveys/SurveyList';
-// import * as actions from '../actions';
 import { connect } from 'react-redux';
 
-class Dashboard extends Component {
-   showLink() {
-      if (this.props.auth) {
-         if (this.props.auth.credits < 1) {
+const Dashboard = ({ auth }) => {
+   const showLink = () => {
+      if (auth) {
+         if (auth.credits < 1) {
             return (
                <h4 style={{ textAlign: 'center', paddingTop: '20px' }}>
                   <p>Welcome to VoteNow!</p>
@@ -26,11 +23,11 @@ class Dashboard extends Component {
          }
       }
       return;
-   }
+   };
 
-   showAddButton() {
-      if (this.props.auth) {
-         if (this.props.auth.credits) {
+   const showAddButton = () => {
+      if (auth) {
+         if (auth.credits) {
             return (
                <div className="fixed-action-btn">
                   <Link
@@ -44,36 +41,19 @@ class Dashboard extends Component {
          }
       }
       return;
-   }
+   };
 
-   render() {
-      return (
-         <div>
-            {this.showLink()}
-            <SurveyList />
-            {this.showAddButton()}
-         </div>
-      );
-   }
-}
+   return (
+      <div>
+         {showLink()}
+         <SurveyList />
+         {showAddButton()}
+      </div>
+   );
+};
 
-function mapStateToProps({ auth }) {
+const mapStateToProps = ({ auth }) => {
    return { auth };
-}
+};
 
 export default connect(mapStateToProps)(Dashboard);
-
-// const Dashboard = () => {
-//    return (
-//       <div>
-//          <SurveyList />
-//          <div className="fixed-action-btn">
-//             <Link to="/surveys/new" className="btn-floating btn-large green">
-//                <i className="material-icons">add</i>
-//             </Link>
-//          </div>
-//       </div>
-//    );
-// };
-
-// export default Dashboard;
